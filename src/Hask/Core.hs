914d20a443b8851dc5938ed9cfc32ad760c511dc
Edward Kmett <ekmett@gmail.com>
2014-07-12 04:46:46 -0400

    Folded Hask.Kan back into Hask.Core

--- src/Hask/Core.hs
+++ src/Hask/Core.hs
@@ -1981,3 +1981,42 @@ instance Colim2 -| Const2 where
 instance Cocomplete (Nat :: (i -> *) -> (i -> *) -> *) where
   type Colim = Colim2
   colim = Nat Colim2
+
+-- * Kan extensions
+
+class (c ~ Hom) => HasRan (c :: k -> k -> *) | k -> c where
+  type Ran :: (i -> j) -> (i -> k) -> j -> k
+  ranDict :: Dict (Curried Compose (Ran :: (i -> j) -> (i -> k) -> j -> k))
+  default ranDict :: Curried Compose (Ran :: (i -> j) -> (i -> k) -> j -> k) => Dict (Curried Compose (Ran :: (i -> j) -> (i -> k) -> j -> k))
+  ranDict = Dict
+
+data Ran1 f g a = forall z. Ran (Compose z f ~> g) (z a)
+
+instance Curried Compose1 Ran1 where
+  curry l = Nat (Ran l)
+  uncurry l = Nat $ \ (Compose ab) -> case runNat l ab of
+    Ran czfg za -> runNat czfg (Compose za)
+
+instance HasRan (->) where
+  type Ran = Ran1
+
+instance Category (Hom :: j -> j -> *) => Functor (Ran1 f :: (i -> *) -> (j -> *)) where
+  fmap f = Nat $ \(Ran k z) -> Ran (f . k) z
+
+class (c ~ Hom) => HasLan (c :: k -> k -> *) | k -> c where
+  type Lan :: (i -> j) -> (i -> k) -> j -> k
+  lanDict :: Dict (Cocurried (Lan :: (i -> j) -> (i -> k) -> j -> k) Compose)
+  default lanDict :: Cocurried (Lan :: (i -> j) -> (i -> k) -> j -> k) Compose => Dict (Cocurried (Lan :: (i -> j) -> (i -> k) -> j -> k) Compose)
+  lanDict = Dict
+
+newtype Lan1 f g a = Lan { runLan :: forall z. (g ~> Compose z f) ~> z a }
+
+instance Cocurried Lan1 Compose1 where
+  cocurry l = Nat $ \b -> Compose $ runNat l (Lan $ \f -> case runNat f b of Compose z -> z)
+  uncocurry k = Nat $ \xs -> runLan xs k
+
+instance HasLan (->) where
+  type Lan = Lan1
+
+instance Category (Hom :: j -> j -> *) => Functor (Lan1 f :: (i -> *) -> (j -> *)) where
+  fmap f = Nat $ \l -> Lan $ \k -> runLan l (k . f)
