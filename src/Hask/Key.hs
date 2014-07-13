d9534d88cde45e1151992e6d284b80ed6a1a2758
Edward Kmett <ekmett@gmail.com>
2014-07-12 21:59:37 -0400

    found an adjunction governing my "dual" of Atkey. More structure to the class. Merged HasAtkey and HasCoatkey into one class

--- src/Hask/Key.hs
+++ src/Hask/Key.hs
@@ -19,82 +19,85 @@ module Hask.Key where
 
 import Hask.Core
 
-ifmap :: (Functor f, Post Functor at) => (a ~> b) -> f (at i a) ~> f (at i b)
-ifmap = fmap . fmap1
+ifmap :: (Functor f, Functor at) => (a ~> b) -> f (at a i) ~> f (at b i)
+ifmap = fmap . first
 
 class (Category hom, hom ~ Hom) => HasAtkey (hom :: y -> y -> *) where
-  type Atkey :: x -> y -> x -> y
-  atkey :: forall (a :: y) i. a ~> Atkey i a i
-  atkeyFunctor1 :: Dict (Post Functor (Atkey :: x -> y -> x -> y))
-  default atkeyFunctor1 :: Post Functor (Atkey :: x -> y -> x -> y) => Dict (Post Functor (Atkey :: x -> y -> x -> y))
-  atkeyFunctor1 = Dict
+  type Atkey :: y -> x -> x -> y
+  atkey :: forall (a :: y) i. a ~> Atkey a i i
+  ibind :: forall (m :: (x -> y) -> x -> y) (a :: y) (bk :: x -> y) (i :: x) (j :: x). Monad m => (a ~> m bk j) -> m (Atkey a j) i ~> m bk i
+  ireturn :: (Monoidal m, Strength m) => hom a (m (Atkey a i) i)
 
-  -- ibind :: Monad m => (a ~> m (Atkey k b) j) ~> m (Atkey j a) i ~> m (Atkey k b) i
-  ibind :: forall (m :: (x -> y) -> x -> y) (a :: y) (kb :: x -> y) (i :: x) (j :: x). Monad m => (a ~> m kb j) -> m (Atkey j a) i ~> m kb i
-  ireturn :: (Monoidal m, Strength m) => hom a (m (Atkey i a) i)
+  atkeyComonoidal :: Dict (Comonoidal (Atkey :: y -> x -> x -> y))
+  default atkeyComonoidal :: Comonoidal (Atkey :: y -> x -> x -> y) => Dict (Comonoidal (Atkey :: y -> x -> x -> y))
+  atkeyComonoidal = Dict
 
--- Conor McBride's "Atkeykey" adapted to this formalism
-data Atkey0 i a j where
-  Atkey :: a -> Atkey0 i a i
+  -- The dual of Conor McBride's "Atkey" adapted to this formalism
+  type Coatkey :: y -> x -> x -> y
+  coatkey :: forall (a :: y) (i :: x). hom (Coatkey a i i) a
+
+  coatkeyMonoidal :: Dict (Monoidal (Coatkey :: y -> x -> x -> y))
+  default coatkeyMonoidal :: Monoidal (Coatkey :: y -> x -> x -> y) => Dict (Monoidal (Coatkey :: y -> x -> x -> y))
+  coatkeyMonoidal = Dict
+
+  iextend :: forall (w :: (x -> y) -> x -> y) (ak :: x -> y) (i :: x) (j :: x) (b :: y). Comonad w => (w ak j ~> b) -> w ak i ~> w (Coatkey b j) i
+
+  iextract :: Comonad w => hom (w (Coatkey a i) i) a
+  iextract = coatkey . runNat extract
+
+  -- There is an adjunction between the obligations of Atkey and the problem solved by Coatkey
+  atkeyAdj :: forall (a :: y) (b :: y) (a' :: y) (b' :: y) (i :: x) (j :: x) (i' :: x') (j' :: x').
+    Iso (Atkey a i j ~> b) (Atkey a' i' j' ~> b')
+        (a ~> Coatkey b i j)   (a' ~> Coatkey b' i' j')
+
+-- Conor McBride's "Atkey" adapted to this formalism
+data Atkey0 a i j where
+  Atkey :: a -> Atkey0 a i i
+
+newtype Coatkey0 a i j = Coatkey { runCoatkey :: (i ~ j) => a }
 
 instance HasAtkey (->) where
   type Atkey = Atkey0
   atkey = Atkey
-  atkeyFunctor1 = Dict
   ibind f = runNat (bind (Nat (\(Atkey a) -> f a)))
   ireturn a = runNat return (atkey a) -- we can't point-free this one currently in GHC, so we need it in the class
+  atkeyComonoidal = Dict
 
-instance Functor (Atkey0 i) where
-  fmap f = Nat $ \(Atkey a) -> Atkey (f a)
-
-instance Cosemimonoidal (Atkey0 i) where
-  op2 = Nat $ \(Atkey eab) -> Lift (bimap Atkey Atkey eab)
-
-instance Comonoidal (Atkey0 i) where
-  op0 = Nat $ \(Atkey v) -> Const v
-
-instance Cosemigroup m => Cosemigroup (Atkey0 i m) where
-  comult = comultOp
-
-instance Comonoid m => Comonoid (Atkey0 i m) where
-  zero = zeroOp
-
-class (Category hom, hom ~ Hom) => HasCoatkey (hom :: y -> y -> *) where
-  -- The dual of Conor McBride's "Atkeykey" adapted to this formalism
-  type Coatkey :: x -> y -> x -> y
-  coatkey :: forall (a :: y) (i :: x). hom (Coatkey i a i) a
-
-  coatkeyFunctor1 :: Dict (Post Functor (Coatkey :: x -> y -> x -> y))
-  default coatkeyFunctor1 :: Post Functor (Coatkey :: x -> y -> x -> y) => Dict (Post Functor (Coatkey :: x -> y -> x -> y))
-  coatkeyFunctor1 = Dict
-
-  iextend :: forall (w :: (x -> y) -> x -> y) (ka :: x -> y) (i :: x) (j :: x) (b :: y). Comonad w => (w ka j ~> b) -> w ka i ~> w (Coatkey j b) i
-
-  iextract :: Comonad w => hom (w (Coatkey i a) i) a
-  iextract = coatkey . runNat extract
-
-newtype Coatkey0 i a j = Coatkey { runCoatkey :: (i ~ j) => a }
-
-instance HasCoatkey (->) where
   type Coatkey = Coatkey0
   coatkey = runCoatkey
-  coatkeyFunctor1 = Dict
+  coatkeyMonoidal = Dict
   iextend f = runNat (extend (Nat (\a -> Coatkey (f a))))
 
-instance Functor (Coatkey0 i) where
-  fmap f = Nat $ \xs -> Coatkey $ f (runCoatkey xs)
+  atkeyAdj = dimap (\aijb a -> Coatkey $ aijb $ Atkey a) (\abij (Atkey a) -> runCoatkey (abij a))
 
-instance Semimonoidal (Coatkey0 i) where
-  ap2 = Nat $ \ab -> Coatkey $ case ab of
-    Lift (Coatkey a, Coatkey b) -> (a, b)
+instance Functor Atkey0 where
+  fmap f = nat2 $ \(Atkey a) -> Atkey (f a)
 
-instance Monoidal (Coatkey0 i) where
-  ap0 = Nat $ \a -> Coatkey (getConst a)
+instance Cosemimonoidal Atkey0 where
+  op2 = nat2 $ \(Atkey eab) -> Lift2 $ Lift $ bimap Atkey Atkey eab
 
-instance Semigroup m => Semigroup (Coatkey0 i m) where
+instance Comonoidal Atkey0 where
+  op0 = nat2 $ \(Atkey v) -> Const2 $ Const v
+
+instance Cosemigroup m => Cosemigroup (Atkey0 m) where
+  comult = comultOp
+
+instance Comonoid m => Comonoid (Atkey0 m) where
+  zero = zeroOp
+
+instance Functor Coatkey0 where
+  fmap f = nat2 $ \xs -> Coatkey $ f (runCoatkey xs)
+
+instance Semimonoidal Coatkey0 where
+  ap2 = nat2 $ \ab -> Coatkey $ case ab of
+    Lift2 (Lift (Coatkey a, Coatkey b)) -> (a, b)
+
+instance Monoidal Coatkey0 where
+  ap0 = nat2 $ \a -> Coatkey (getConst (getConst2 a))
+
+instance Semigroup m => Semigroup (Coatkey0 m) where
   mult = multM
 
-instance Monoid m => Monoid (Coatkey0 i m) where
+instance Monoid m => Monoid (Coatkey0 m) where
   one = oneM
 
--- atkeyAdj :: Iso' (Atkey i a j ~> b) (a ~> Coatkey i b j) -- if we shuffle the args we can make this a normal adjunction in Nat x -> y -> z
