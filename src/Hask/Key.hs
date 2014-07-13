2f6f55e6807370990c599ab780a7ebf67d328d52
Edward Kmett <ekmett@gmail.com>
2014-07-12 21:16:12 -0400

    more work fleshing out Hask.Key

--- src/Hask/Key.hs
+++ src/Hask/Key.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 --------------------------------------------------------------------
 -- |
@@ -15,46 +19,74 @@ module Hask.Key where
 
 import Hask.Core
 
--- The dual of Conor McBride's "Atkey" adapted to this formalism
---
--- Cokey i :: Hask -> Nat
--- Cokey :: x -> * -> x -> *
-newtype Cokey i a j = Cokey { runCokey :: (i ~ j) => a }
+ifmap :: (Functor f, Post Functor at) => (a ~> b) -> f (at i a) ~> f (at i b)
+ifmap = fmap . fmap1
 
-instance Functor (Cokey i) where
-  fmap f = Nat $ \xs -> Cokey $ f (runCokey xs)
+class (hom ~ Hom) => HasAtkey (hom :: y -> y -> *) where
+  type Atkey :: x -> y -> x -> y
+  atkey :: forall (a :: y) i. a ~> Atkey i a i
+  atkeyFunctor1 :: Dict (Post Functor (Atkey :: x -> y -> x -> y))
+  default atkeyFunctor1 :: Post Functor (Atkey :: x -> y -> x -> y) => Dict (Post Functor (Atkey :: x -> y -> x -> y))
+  atkeyFunctor1 = Dict
 
-instance Semimonoidal (Cokey i) where
-  ap2 = Nat $ \ab -> Cokey $ case ab of
-    Lift (Cokey a, Cokey b) -> (a, b)
+-- Conor McBride's "Atkeykey" adapted to this formalism
+data Atkey0 i a j where
+  Atkey :: a -> Atkey0 i a i
 
-instance Monoidal (Cokey i) where
-  ap0 = Nat $ \a -> Cokey (getConst a)
+instance HasAtkey (->) where
+  type Atkey = Atkey0
+  atkey = Atkey
+  atkeyFunctor1 = Dict
 
-instance Semigroup m => Semigroup (Cokey i m) where
-  mult = multM
+instance Functor (Atkey0 i) where
+  fmap f = Nat $ \(Atkey a) -> Atkey (f a)
 
-instance Monoid m => Monoid (Cokey i m) where
-  one = oneM
+instance Cosemimonoidal (Atkey0 i) where
+  op2 = Nat $ \(Atkey eab) -> Lift (bimap Atkey Atkey eab)
 
--- Conor McBride's "Atkey" adapted to this formalism
---
--- Key i :: Hask -> Nat
--- Key :: x -> * -> x -> *
-data Key i a j where
-  Key :: a -> Key i a i
+instance Comonoidal (Atkey0 i) where
+  op0 = Nat $ \(Atkey v) -> Const v
 
-instance Functor (Key i) where
-  fmap f = Nat $ \ (Key a) -> Key (f a)
-
-instance Cosemimonoidal (Key i) where
-  op2 = Nat $ \(Key eab) -> Lift (bimap Key Key eab)
-
-instance Comonoidal (Key i) where
-  op0 = Nat $ \(Key v) -> Const v
-
-instance Cosemigroup m => Cosemigroup (Key i m) where
+instance Cosemigroup m => Cosemigroup (Atkey0 i m) where
   comult = comultOp
 
-instance Comonoid m => Comonoid (Key i m) where
+instance Comonoid m => Comonoid (Atkey0 i m) where
   zero = zeroOp
+
+ireturn :: (Monoidal m, Strength m) => a ~> m (Atkey0 i a) i
+ireturn a = runNat return (Atkey a)
+
+-- ibind :: Monad m => (a ~> m (Atkey k b) j) ~> m (Atkey j a) i ~> m (Atkey k b) i
+--ibind :: Monad m => (a ~> m atkb j) ~> m (Atkey0 j a) i ~> m atkb i
+--ibind f = bind (\(Atkey a) -> f a)
+
+newtype Coatkey0 i a j = Coatkey { runCoatkey :: (i ~ j) => a }
+
+class hom ~ Hom => HasCoatkey (hom :: y -> y -> *) where
+  -- The dual of Conor McBride's "Atkeykey" adapted to this formalism
+  type Coatkey :: x -> y -> x -> y
+  coatkey :: forall (a :: y) i. Coatkey i a i ~> a
+  coatkeyFunctor1 :: Dict (Post Functor (Coatkey :: x -> y -> x -> y))
+  default coatkeyFunctor1 :: Post Functor (Coatkey :: x -> y -> x -> y) => Dict (Post Functor (Coatkey :: x -> y -> x -> y))
+  coatkeyFunctor1 = Dict
+
+instance Functor (Coatkey0 i) where
+  fmap f = Nat $ \xs -> Coatkey $ f (runCoatkey xs)
+
+instance Semimonoidal (Coatkey0 i) where
+  ap2 = Nat $ \ab -> Coatkey $ case ab of
+    Lift (Coatkey a, Coatkey b) -> (a, b)
+
+instance Monoidal (Coatkey0 i) where
+  ap0 = Nat $ \a -> Coatkey (getConst a)
+
+instance Semigroup m => Semigroup (Coatkey0 i m) where
+  mult = multM
+
+instance Monoid m => Monoid (Coatkey0 i m) where
+  one = oneM
+
+iextract :: Comonad w => w (Coatkey0 i a) i ~> a
+iextract = runCoatkey . runNat extract
+
+-- instance Atkey0 x -| Coatkey0 x where
