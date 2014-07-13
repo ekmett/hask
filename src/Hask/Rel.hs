2295eae186e089013795a58793bd797a771c3b82
Edward Kmett <ekmett@gmail.com>
2014-07-12 23:25:59 -0400

    document reflectors and coreflectors

--- src/Hask/Rel.hs
+++ src/Hask/Rel.hs
@@ -4,7 +4,15 @@ module Hask.Rel where
 
 import Hask.Core
 
--- This provides a canonical faithful functor that is injective on objects.
+-- | Rel should be a faithful functor that is also injective on objects.
+--
+-- and it sometimes has adjoints, if either exist, then they should satisfy the following relationship
+--
+-- @
+-- 'Reflected' '-|' 'Rel' '-|' 'Coreflected'
+-- @
+--
+-- This provides the inclusion functor for a subcategory with objects indexed in @i@ into a category with objects in @j@
 type family Rel :: i -> j
 type instance Rel = Const1
 type instance Rel = Const2
@@ -13,12 +21,12 @@ type instance Rel = Id1 -- full
 type instance Rel = IdC -- full
 -- type instance Rel = Id2 -- full
 
--- Rel is a faithful functor
+-- | The <http://en.wikipedia.org/wiki/Reflective_subcategory "reflector"> of a
+-- reflective sub-category with objects in @j@ of a category with objects in @i@.
 --
--- and it sometimes has adjoints, if both exist, then these should satisfy the following relationship
---
--- Reflected -| Rel -| Coreflected
-
+-- @
+-- 'Reflected' '-|' 'Rel'
+-- @
 type family Reflected :: j -> i
 type instance Reflected = Colim1
 type instance Reflected = Colim2
@@ -26,6 +34,11 @@ type instance Reflected = Id1
 type instance Reflected = IdC
 -- type instance Reflected = Id2
 
+-- | This <http://en.wikipedia.org/wiki/Reflective_subcategory "coreflector"> of a coreflective sub-category with objects in @j@ of a category with objects in @i@.
+--
+-- @
+-- 'Rel' '-|' 'Coreflected'
+-- @
 type family Coreflected :: j -> i
 type instance Coreflected = Lim1
 type instance Coreflected = Lim2
