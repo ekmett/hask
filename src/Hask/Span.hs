0888c3fd0acc217ed82b6d603631c60a5196889c
Edward Kmett <ekmett@gmail.com>
2014-07-18 07:03:49 -0400

    spans in Nat and (:-)

--- src/Hask/Span.hs
+++ src/Hask/Span.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE GADTs #-}
@@ -40,22 +41,54 @@ type Pullback f g x y z  = Lim (Cospan f g x y z)
 type Coequalizer f g x z = Pushout f g x z z
 type Equalizer f g x z   = Pullback f g x z z
 
--- Const a provides the trivial span when the index is set to come from (Λ)
-
 type family XYZ (x :: i) (y :: i) (z :: i) (w :: Λ) :: i where
   XYZ x y z 'X = x
   XYZ x y z 'Y = y
   XYZ x y z 'Z = z
 
-type family Span :: l -> r -> i -> i -> i -> Λ -> i
+class (h ~ Hom) => Spanned (h :: i -> i -> *) where
+  type family Span :: a -> b -> i -> i -> i -> Λ -> i
+  spanFunctor :: (Reifies l (h x y), Reifies r (h x z)) => Dict (Functor (Span l r x y z))
+  default spanFunctor :: (Reifies l (h x y), Reifies r (h x z), Functor (Span l r x y z)) => Dict (Functor (Span l r x y z))
+  spanFunctor = Dict
+
+  _Span :: forall (l :: a) (r :: b) (l' :: a') (r' :: b') (x :: i) (x' :: i) (y :: i) (y' :: i) (z :: i) (z' :: i) (w :: Λ) (w' :: Λ).
+    Iso (Span l r x y z w) (Span l' r' x' y' z' w') (XYZ x y z w) (XYZ x' y' z' w')
+
+-- | This provides a nicely behaved canonical functor from Λ to *
 newtype Span0 l r x y z w = Span { runSpan :: XYZ x y z w }
-type instance Span = Span0
-_Span = dimap runSpan Span
+
+instance Spanned (->) where
+  type Span = Span0
+  _Span = dimap runSpan Span
 
 instance (Reifies l (x -> y), Reifies r (x -> z)) => Functor (Span0 l r x y z) where
-  fmap I w = w
-  fmap L (Span x) = Span (reflect (Proxy :: Proxy l) x)
-  fmap R (Span x) = Span (reflect (Proxy :: Proxy r) x)
+  fmap I = id
+  fmap L = _Span $ reflect (Proxy :: Proxy l)
+  fmap R = _Span $ reflect (Proxy :: Proxy r)
+
+newtype Span1 l r x y z w a = Span1 { runSpan1 :: XYZ x y z w a }
+
+instance Spanned (Nat :: (i -> *) -> (i -> *) -> *) where
+  type Span = Span1
+  _Span = dimap (Nat runSpan1) (Nat Span1)
+
+instance (Reifies l (x ~> y), Reifies r (x ~> z)) => Functor (Span1 l r x y z) where
+  fmap I = id
+  fmap L = _Span $ reflect (Proxy :: Proxy l)
+  fmap R = _Span $ reflect (Proxy :: Proxy r)
+
+class XYZ x y z w => SpanC l r x y z w
+instance XYZ x y z w => SpanC l r x y z w
+
+instance Spanned (:-) where
+  type Span = SpanC
+  _Span = dimap (Sub Dict) (Sub Dict)
+
+instance (Reifies l (x :- y), Reifies r (x :- z)) => Functor (SpanC l r x y z) where
+  fmap I = id
+  fmap L = _Span $ reflect (Proxy :: Proxy l)
+  fmap R = _Span $ reflect (Proxy :: Proxy r)
 
 data ReifiedId a = ReifiedId
 instance Reifies (ReifiedId a) (a -> a) where reflect _ = id
@@ -78,3 +111,4 @@ instance (Reifies l (y -> x), Reifies r (z -> x)) => Contravariant (Cospan0 l r 
   contramap L (Cospan y) = Cospan (reflect (Proxy :: Proxy l) y)
   contramap R (Cospan z) = Cospan (reflect (Proxy :: Proxy r) z)
 
+-- TODO: generalized (co)equalizers 
