a1fbfd04245173dd43821c251b556b710e04e826
Edward Kmett <ekmett@gmail.com>
2014-07-30 13:01:20 -0400

    Renaming Op -> Yoneda, UnOp -> Op

--- wip/Univariant.hs
+++ wip/Univariant.hs
@@ -21,17 +21,17 @@ import Unsafe.Coerce (unsafeCoerce)
 -- You should produce instances of 'Category'' and consume instances of 'Category'.
 class Category' (p :: i -> i -> *) where
   type Ob p :: i -> Constraint
-  id      :: Ob p a => p a a
+  id :: Ob p a => p a a
   observe :: p a b -> Dict (Ob p a, Ob p b)
-  (.)     :: p b c -> p a b -> p a c
-  unop :: UnOp p b a -> p a b
-  op   :: p a b -> UnOp p b a
+  (.) :: p b c -> p a b -> p a c
 
-  -- default unop :: (UnOp p ~ Op p) => Opd p b a -> p a b
-  -- unop = getOp
+  op :: Op p b a -> p a b
+  default op :: Op p ~ Yoneda p => Op p b a -> p a b
+  op = getYoneda
 
-  -- default op :: (Opd p ~ Op p) => Opd p b a -> p a b
-  -- op = getOp
+  unop :: p b a -> Op p a b
+  default unop :: Op p ~ Yoneda p => p b a -> Op p a b
+  unop = Yoneda
 
 --------------------------------------------------------------------------------
 -- * Functors
@@ -77,22 +77,22 @@ bimap f g = case observe f of
 -- * Contravariance
 --------------------------------------------------------------------------------
 
-newtype Op (p :: i -> i -> *) (a :: i) (b :: i) = Op { getOp :: p b a }
+newtype Yoneda (p :: i -> i -> *) (a :: i) (b :: i) = Yoneda { getYoneda :: p b a }
 
-type family UnOp (p :: i -> i -> *) :: i -> i -> * where
-  UnOp (Op p) = p
-  UnOp p = Op p
+type family Op (p :: i -> i -> *) :: i -> i -> * where
+  Op (Yoneda p) = p
+  Op p = Yoneda p
 
-type Opd f = UnOp (Dom f)
+type Opd f = Op (Dom f)
 
-class (Dom p ~ UnOp (Opd p)) => Contra p
-instance (Dom p ~ UnOp (Opd p)) => Contra p
+class (Dom p ~ Op (Opd p)) => Contra p
+instance (Dom p ~ Op (Opd p)) => Contra p
 
 class (Contra f, Functor f) => Contravariant f
 instance (Contra f, Functor f) => Contravariant f
 
 contramap :: Contravariant f => Opd f b a -> Cod f (f a) (f b)
-contramap = fmap . unop
+contramap = fmap . op
 
 
 --------------------------------------------------------------------------------
@@ -103,15 +103,15 @@ class (Contra f, Bifunctor f) => Profunctor f
 instance (Contra f, Bifunctor f) => Profunctor f
 
 dimap :: Profunctor p => Opd p b a -> Dom2 p c d -> Cod2 p (p a c) (p b d)
-dimap = bimap . unop
+dimap = bimap . op
 
 type Iso
   (c :: i -> i -> *) (d :: j -> j -> *) (e :: k -> k -> *)
   (s :: i) (t :: j) (a :: i) (b :: j) = forall (p :: i -> j -> k).
-  (Profunctor p, Dom p ~ UnOp c, Dom2 p ~ d, Cod2 p ~ e) => e (p a b) (p s t)
+  (Profunctor p, Dom p ~ Op c, Dom2 p ~ d, Cod2 p ~ e) => e (p a b) (p s t)
 
-class (Category' p, Profunctor p, Dom p ~ UnOp p, p ~ UnOp (UnOp p), Cod p ~ Nat p (->), Dom2 p ~ p, Cod2 p ~ (->), Functor' (Ob p)) => Category p
-instance (Category' p, Profunctor p, Dom p ~ UnOp p, p ~ UnOp (UnOp p), Cod p ~ Nat p (->), Dom2 p ~ p, Cod2 p ~ (->), Functor' (Ob p)) => Category p
+class (Category' p, Category' (Op p), Profunctor p, Dom p ~ Op p, p ~ Op (Op p), Cod p ~ Nat p (->), Dom2 p ~ p, Cod2 p ~ (->), Functor' (Ob p)) => Category p
+instance (Category' p, Category' (Op p), Profunctor p, Dom p ~ Op p, p ~ Op (Op p), Cod p ~ Nat p (->), Dom2 p ~ p, Cod2 p ~ (->), Functor' (Ob p)) => Category p
 
 --------------------------------------------------------------------------------
 -- * Vacuous Constraints
@@ -137,7 +137,7 @@ instance (Ob c ~ Vacuous c) => Functor' (Vacuous c) where
 instance Functor' (:-) where
   type Dom (:-) = Op (:-)
   type Cod (:-) = Nat (:-) (->) -- copresheaves
-  fmap (Op f) = Nat (. f)
+  fmap (Yoneda f) = Nat (. f)
 
 instance Functor' ((:-) b) where
   type Dom ((:-) a) = (:-)
@@ -149,8 +149,7 @@ instance Category' (:-) where
   id = Constraint.refl
   observe _ = Dict
   (.) = Constraint.trans
-  unop = getOp
-  op = Op
+  op = getYoneda
 
 constraint :: Dict (Category (:-))
 constraint = Dict
@@ -162,7 +161,7 @@ constraint = Dict
 instance Functor' (->) where
   type Dom (->) = Op (->)
   type Cod (->) = Nat (->) (->)
-  fmap (Op f) = Nat (. f)
+  fmap (Yoneda f) = Nat (. f)
 
 instance Functor' ((->)a) where
   type Dom ((->) a) = (->)
@@ -174,39 +173,37 @@ instance Category' (->) where
   id x = x
   observe _ = Dict
   (.) f g x = f (g x)
-  unop = getOp
-  op = Op
+  op = getYoneda
 
 hask :: Dict (Category (->))
 hask = Dict
 
 --------------------------------------------------------------------------------
--- * Op
+-- * Yoneda
 --
--- Op is basically Yoneda :: i -> [ Op i, Set ]
--- except the first argument is double negated: Op (Op i) -> [ Op i, Set ]
+-- Yoneda :: i -> [ Op i, Set ]
 --------------------------------------------------------------------------------
 
-instance (Category p, UnOp p ~ Op p) => Functor' (Op p) where
-  type Dom (Op p) = p -- Op (Op p)
-  type Cod (Op p) = Nat (Op p) (->)
-  fmap f = Nat (. Op f)
+instance (Category p, Op p ~ Yoneda p) => Functor' (Yoneda p) where
+  type Dom (Yoneda p) = p
+  type Cod (Yoneda p) = Nat (Yoneda p) (->)
+  fmap f = Nat (. Yoneda f)
 
-instance (Category p, UnOp p ~ Op p) => Functor' (Op p a) where
-  type Dom (Op p a) = Op p
-  type Cod (Op p a) = (->)
+instance (Category p, Op p ~ Yoneda p) => Functor' (Yoneda p a) where
+  type Dom (Yoneda p a) = Yoneda p
+  type Cod (Yoneda p a) = (->)
   fmap = (.)
 
-instance (Category p, UnOp p ~ Op p) => Category' (Op p) where
-  type Ob (Op p) = Ob p
-  id = Op id
-  Op f . Op g = Op (g . f)
-  observe (Op f) = case observe f of
+instance (Category p, Op p ~ Yoneda p) => Category' (Yoneda p) where
+  type Ob (Yoneda p) = Ob p
+  id = Yoneda id
+  Yoneda f . Yoneda g = Yoneda (g . f)
+  observe (Yoneda f) = case observe f of
     Dict -> Dict
-  unop = op
-  op = getOp
+  op = Yoneda
+  unop = getYoneda
 
-opDict :: (Category p, UnOp p ~ Op p) => Dict (Category (Op p))
+opDict :: (Category p, Op p ~ Yoneda p) => Dict (Category (Yoneda p))
 opDict = Dict
 
 --------------------------------------------------------------------------------
@@ -221,7 +218,7 @@ data Nat (p :: i -> i -> *) (q :: j -> j -> *) (f :: i -> j) (g :: i -> j) where
          } -> Nat p q f g
 
 type Copresheaves p = Nat p (->)
-type Presheaves p = Nat (UnOp p) (->)
+type Presheaves p = Nat (Op p) (->)
 
 class (Functor f, Dom f ~ p, Cod f ~ q) => FunctorOf p q f
 instance (Functor f, Dom f ~ p, Cod f ~ q) => FunctorOf p q f
@@ -234,7 +231,7 @@ instance Functor' (FunctorOf p q) where
 instance (Category' p, Category q) => Functor' (Nat p q) where
   type Dom (Nat p q) = Op (Nat p q)
   type Cod (Nat p q) = Nat (Nat p q) (->)
-  fmap (Op f) = Nat (. f)
+  fmap (Yoneda f) = Nat (. f)
 
 instance (Category' p, Category q) => Functor' (Nat p q a) where
   type Dom (Nat p q f) = Nat p q
@@ -248,8 +245,7 @@ instance (Category' p, Category' q) => Category' (Nat p q) where
      id1 = id \\ (ob :: Ob p x :- Ob q (f x))
    observe Nat{} = Dict
    Nat f . Nat g = Nat (f . g)
-   unop = getOp
-   op = Op
+   op = getYoneda
 
 nat :: (Category p, Category q) => Dict (Category (Nat p q))
 nat = Dict
@@ -433,8 +429,8 @@ instance Category c => Functor' (Get c) where
 instance (Category c, Ob c r) => Functor' (Get c r) where
   type Dom (Get c r) = Op c
   type Cod (Get c r) = Nat c (->)
-  fmap (Op f) = case observe f of
-    Dict -> Nat $ _Get $ (. f)
+  fmap f = case observe (unop f) of
+    Dict -> Nat $ _Get $ (. op f)
 
 instance (Category c, Ob c r, Ob c a) => Functor' (Get c r a) where
   type Dom (Get c r a) = c
@@ -455,13 +451,13 @@ _Beget = dimap runBeget Beget
 
 instance Category c => Functor' (Beget c) where
   type Dom (Beget c) = Op c
-  type Cod (Beget c) = Nat (Op c) (Nat c (->))
-  -- fmap (Op f) = Nat $ Nat $ _Beget (. f) -- TODO
+  type Cod (Beget c) = Nat (Yoneda c) (Nat c (->))
+  -- fmap (Yoneda f) = Nat $ Nat $ _Beget (. f) -- TODO
 
 instance (Category c, Ob c r) => Functor' (Beget c r) where
   type Dom (Beget c r) = Op c
   type Cod (Beget c r) = Nat c (->)
-  fmap (Op f) = case observe f of
+  fmap f = case observe (unop f) of
     Dict -> Nat $ _Beget id
 
 instance (Category c, Ob c r, Ob c a) => Functor' (Beget c r a) where
@@ -530,8 +526,8 @@ associateCompose = dimap (Nat (beget _Compose . fmap (beget _Compose) . get _Com
 
 type Prof c d e = Nat (Op c) (Nat d e)
 
-class    (Profunctor f, Dom f ~ UnOp p, Dom2 f ~ q, Cod2 f ~ r) => ProfunctorOf p q r f
-instance (Profunctor f, Dom f ~ UnOp p, Dom2 f ~ q, Cod2 f ~ r) => ProfunctorOf p q r f
+class    (Profunctor f, Dom f ~ Op p, Dom2 f ~ q, Cod2 f ~ r) => ProfunctorOf p q r f
+instance (Profunctor f, Dom f ~ Op p, Dom2 f ~ q, Cod2 f ~ r) => ProfunctorOf p q r f
 
 -- TODO: strip off f just to get basic unenriched profunctors to work
 
