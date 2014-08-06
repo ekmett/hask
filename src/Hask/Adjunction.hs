f11c9f45e6952c910acc6a22bf1620396c8a3209
Edward Kmett <ekmett@gmail.com>
2014-08-06 01:18:59 -0400

    Adjunctions

---
+++ src/Hask/Adjunction.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE PolyKinds, KindSignatures, MultiParamTypeClasses, FunctionalDependencies, TypeFamilies, TypeOperators #-}
+module Hask.Adjunction 
+  ( (-|)(..)
+  , swap
+  , Curried(..)
+  ) where
+
+import Hask.Category
+import qualified Prelude
+
+--------------------------------------------------------------------------------
+-- * Adjunctions
+--------------------------------------------------------------------------------
+
+class (Functor f, Functor g, Dom f ~ Cod g, Cod g ~ Dom f) => (f :: j -> i) -| (g :: i -> j) | f -> g, g -> f where
+  adj :: Iso (->) (->) (->) (Cod f (f a) b) (Cod f (f a') b') (Cod g a (g b)) (Cod g a' (g b'))
+
+instance (,) e -| (->) e where
+  adj = dimap (. swap) (. swap) . curried
+
+swap :: (a,b) -> (b,a)
+swap (a,b) = (b,a)
+
+--------------------------------------------------------------------------------
+-- * Currying
+--------------------------------------------------------------------------------
+
+class (Bifunctor p, Bifunctor q) => Curried (p :: k -> i -> j) (q :: i -> j -> k) | p -> q, q -> p where
+  curried :: Iso (->) (->) (->)
+    (Dom2 p (p a b) c) (Dom2 p (p a' b') c')
+    (Dom2 q a (q b c)) (Dom2 q a' (q b' c'))
+
+instance Curried (,) (->) where
+  curried = dimap Prelude.curry Prelude.uncurry
