58f47ceb54137becd3c5f74dcd8eb312625339db
echatav <eitan.chatav@gmail.com>
2014-08-25 20:34:55 -0700

    associativity of Day convolution

--- wip/Univariant.hs
+++ wip/Univariant.hs
@@ -1021,7 +1021,6 @@ instance (Dom t ~ c, Category c) => Functor (Day t) where
     fmap' (Nat natf) = Nat $ Nat $ \(Day c fx gy) -> Day c (natf fx) gy
 
 --Day convolution on a monoidal category is associative making it a Semitensor
---To Do: define Isos
 
 data Day3L (t :: i -> i -> i) (f :: i -> *) (g :: i -> *) (h :: i -> *) (a :: i) where
   Day3L :: (Dom t ~ c, CopresheafOf c f, CopresheafOf c g, CopresheafOf c h, Ob c x, Ob c y, Ob c z)
@@ -1031,29 +1030,66 @@ data Day3R (t :: i -> i -> i) (f :: i -> *) (g :: i -> *) (h :: i -> *) (a :: i)
   Day3R :: (Dom t ~ c, CopresheafOf c f, CopresheafOf c g, CopresheafOf c h, Ob c x, Ob c y, Ob c z)
          => c (t x (t y z)) a -> f x -> g y -> h z -> Day3R t f g h a
 
-day3L :: Iso (Copresheaves c) (Copresheaves c) (->)
+instance (Dom t ~ c, CopresheafOf c f, CopresheafOf c g, CopresheafOf c h) => Functor (Day3L t f g h) where
+  type Dom (Day3L t f g h) = Dom t
+  type Cod (Day3L t f g h) = (->)
+  fmap c' (Day3L c fx gy hz) = Day3L (c' . c) fx gy hz
+
+instance (Dom t ~ c, CopresheafOf c f, CopresheafOf c g, CopresheafOf c h) => Functor (Day3R t f g h) where
+  type Dom (Day3R t f g h) = Dom t
+  type Cod (Day3R t f g h) = (->)
+  fmap c' (Day3R c fx gy hz) = Day3R (c' . c) fx gy hz
+
+--Thanks to Shachaf for help with the semitensorClosed constraint
+semitensorClosed :: forall c t x y. (Semitensor t, Category c, Dom t ~ c, Ob c x, Ob c y) => Dict (Ob c (t x y))
+semitensorClosed = case ob :: Ob c x :- FunctorOf c c (t x) of
+  Sub Dict -> case ob :: Ob c y :- Ob c (t x y) of
+    Sub Dict -> Dict
+
+day3 :: (Semitensor t, Dom t ~ c, Category c
+     , CopresheafOf c f, CopresheafOf c g, CopresheafOf c h
+     , CopresheafOf c f', CopresheafOf c g', CopresheafOf c h'
+     ) => Iso (Copresheaves c) (Copresheaves c) (->)
+         (Day3L t f g h) (Day3L t f' g' h')
+         (Day3R t f g h) (Day3R t f' g' h')
+day3 = dimap (Nat hither) (Nat yon)
+  where
+    hither :: (Semitensor t, Dom t ~ c, Category c) => Day3L t f g h a -> Day3R t f g h a
+    hither (Day3L (c :: c (t (t x y) z) a) fx gy hz) = case semitensorClosed :: Dict (Ob c (t y z)) of
+      Dict -> case semitensorClosed :: Dict (Ob c (t x (t y z))) of
+        Dict -> Day3R (c . beget associate) fx gy hz
+    yon :: (Semitensor t, Dom t ~ c, Category c) => Day3R t f g h a -> Day3L t f g h a
+    yon (Day3R (c :: c (t x (t y z)) a) fx gy hz) = case semitensorClosed :: Dict (Ob c (t y z)) of
+      Dict -> case semitensorClosed :: Dict (Ob c (t x (t y z))) of
+        Dict -> Day3L (c . get associate) fx gy hz
+
+dayL :: (Semitensor t, Dom t ~ c, Category c
+     , CopresheafOf c f, CopresheafOf c g, CopresheafOf c h
+     , CopresheafOf c f', CopresheafOf c g', CopresheafOf c h'
+     ) => Iso (Copresheaves c) (Copresheaves c) (->)
          (Day t (Day t f g) h) (Day t (Day t f' g') h')
-         (Day3L t f g h)        (Day3L t f' g' h')
-day3L = undefined
---this should use yoneda
+         (Day3L t f g h)       (Day3L t f' g' h')
+dayL = dimap (Nat hither) (Nat yon)
+  where
+    hither :: (Semitensor t, Dom t ~ c, Category c) => Day t (Day t f g) h a -> Day3L t f g h a
+    hither (Day c' (Day c fx gy) hz) = Day3L (c' . runNat (fmap c)) fx gy hz
+    yon :: (Semitensor t, Dom t ~ c, Category c) => Day3L t f g h a -> Day t (Day t f g) h a
+    yon (Day3L (c :: c (t (t x y) z) a) fx gy hz) = case semitensorClosed :: Dict (Ob c (t x y)) of
+      Dict -> Day c (Day id fx gy) hz
 
-day3R :: Iso (Copresheaves c) (Copresheaves c) (->)
+dayR :: (Semitensor t, Dom t ~ c, Category c
+     , CopresheafOf c f, CopresheafOf c g, CopresheafOf c h
+     , CopresheafOf c f', CopresheafOf c g', CopresheafOf c h'
+     ) => Iso (Copresheaves c) (Copresheaves c) (->)
+         (Day3R t f g h)       (Day3R t f' g' h')
          (Day t f (Day t g h)) (Day t f' (Day t g' h'))
-         (Day3R t f g h)         (Day3R t f' g' h')
-day3R = undefined
---mirror day3L
+dayR = dimap (Nat hither) (Nat yon)
+  where
+    hither :: (Semitensor t, Dom t ~ c, Category c) => Day3R t f g h a -> Day t f (Day t g h) a
+    hither (Day3R (c :: c (t x (t y z)) a) fx gy hz) = case semitensorClosed :: Dict (Ob c (t y z)) of
+      Dict -> Day c fx (Day id gy hz)
+    yon :: (Semitensor t, Dom t ~ c, Category c) => Day t f (Day t g h) a -> Day3R t f g h a
+    yon (Day c' fx (Day c gy hz)) = Day3R (c' . fmap1 c) fx gy hz
 
-day3 :: Semitensor t
-     => Iso (Copresheaves c) (Copresheaves c) (->)
-        (Day3L t f g h) (Day3L t f' g' h')
-        (Day3R t f g h) (Day3R t f' g' h')
-day3 = undefined
---this should use associate
-
-instance (Dom t ~ c, Category c, Semitensor t) => Semitensor (Day t) where
---  associate = (Un day3R) . day3 . day3L
-  associate = undefined
-
---Here's the function giving me trouble. Gotta use something like it to build up day3.
---dayHither :: (Dom t ~ c, Category c, Semitensor t) => Day3L t f g h a -> Day3R t f g h a
---dayHither (Day3L c fx gy hz) = Day3R (c . beget associate) fx gy hz
+instance (Semitensor t, Category (Dom t)) => Semitensor (Day t)
+  where associate = dayL . day3 . dayR
